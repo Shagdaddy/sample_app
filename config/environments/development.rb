ff86056a89269672dbd12ddee0560ed221c0c362
P. Hicks <shagmon@hotmail.com>
2011-10-12 22:59:25 -0400

    email config fix - moved smtp server settings out of environment.rb and into prod.db

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,5 +30,24 @@ SampleApp::Application.configure do
   
   #Added next part to handle email 101211
 
+# PRH added lines below on 101211 to try and get email flowing
+  require 'tlsmail'
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+#  ActionMailer::Base.default_charset = "utf-8"
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "SirShagdaddy@gmail.com",
+  :password             => "2goo4uXX",
+  :authentication       => "plain",
+  :tls                  => true,
+  :enable_starttls_auto => true
+}
+
+# ActionMailer::Base.default_url_options[:host] = "localhost:3000"
   
 end
