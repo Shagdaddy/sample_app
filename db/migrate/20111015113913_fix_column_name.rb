380beb9b79a7381eb3bdc120d161c2a059ba1d5e
P. Hicks <shagmon@hotmail.com>
2011-10-15 09:11:14 -0400

    fixed a migration txt error part 2

--- db/migrate/20111015113913_fix_column_name.rb
+++ db/migrate/20111015113913_fix_column_name.rb
@@ -2,15 +2,15 @@ class FixColumnName < ActiveRecord::Migration
 
   def self.up
     change_table :messages do |t|
-      t.rename :user_name, :name
+#      t.rename :user_name, :name
       t.rename :email_addr, :email
     end
   end
 
   def self.down
     change_table :messages do |t|
-      t.rename :name, :user_name
-      t.rename :eamil, :email_addr
+#      t.rename :name, :user_name
+      t.rename :email, :email_addr
     end
   end
 
