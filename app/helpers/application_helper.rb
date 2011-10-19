e5862e3bbc62bd188f90f1fa140471daa49aff73
P. Hicks <shagmon@hotmail.com>
2011-10-18 23:07:28 -0400

    push logo up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
   
   def logo
-    image_tag("logo.png", :alt => "Sample App", :class => "round")
+    image_tag("vmslytelogo.png", :alt => "VMS Lyte", :class => "round")
   end
 
 end
